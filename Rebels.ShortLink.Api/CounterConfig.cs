eec0553fe1c50e404ce212de40d4c122b0a49d97
ZBoukich <z.boukich@gmail.com>
2024-02-22 17:38:22 +0100

    Added CounterConfig to be used with the counter service

---
+++ Rebels.ShortLink.Api/CounterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rebels.ShortLink.Api
+{
+    public class CounterConfig
+    {
+        [Required]
+        public string Key { get; init; } = null!;
+        [Required]
+        public long? StartingUid { get; init; }
+        [Required]
+        public long? EndingUid { get; init; }
+    }
+}
